0352fc4feb00e459d8b539e5bb909db8aa47b7a9
JacobHumphreys <Jahumphreys2060@gmail.com>
2026-06-19 00:56:12 -0600

    added native input based package and shell and updated readme

--- flake.nix
+++ flake.nix
@@ -13,7 +13,6 @@
     };
 
     dependencies = with pkgs; [
-      python310
       uv
       stdenv.cc.cc.lib
       zlib
@@ -23,36 +22,80 @@
     ];
 
     libraryPaths = pkgs.lib.makeLibraryPath [
-        pkgs.stdenv.cc.cc.lib
-        pkgs.zlib
-        pkgs.libxcb
-        pkgs.libGL
-        pkgs.glib
-      ];
+      pkgs.stdenv.cc.cc.lib
+      pkgs.zlib
+      pkgs.libxcb
+      pkgs.libGL
+      pkgs.glib
+    ];
+
+    pythonWithoutPackages = pkgs.python312;
+
+    pythonWithPackages = pkgs.python312.withPackages (ps:
+      with ps; [
+        pytest
+        fire #0.7.1
+        loguru #0.7.3
+        manga-ocr # 0.1.14
+        natsort #8.4.0
+        numpy #2.4.4
+        opencv-python #4.13.0
+        pillow #12.2.0
+        pyclipper #1.4.0
+        requests #2.33.1
+        scipy #1.17.1
+        setuptools #80.10.1
+        shapely #2.1.2
+        torch #2.12.0
+        torchsummary #1.5.1
+        torchvision #0.27.0
+        transformers #5.5.4
+        tqdm #4.41.0
+        yattag #1.16.1
+      ]);
   in {
-    devShells.${system}.default = pkgs.mkShell {
-      nativeBuildInputs = dependencies ++ [self.packages.${system}.default];
+    devShells.${system} = {
+      default = pkgs.mkShell {
+        nativeBuildInputs = [pythonWithoutPackages] ++ dependencies ++ [self.packages.${system}.default];
 
-      LD_LIBRARY_PATH = libraryPaths;
+        LD_LIBRARY_PATH = libraryPaths;
 
-      shellHook = ''
-      '';
+        shellHook = ''
+        '';
+      };
+
+      mokuro-nix = pkgs.mkShell {
+        nativeBuildInputs = [pythonWithPackages] ++ dependencies ++ [self.packages.${system}.mokuro-nix];
+
+        LD_LIBRARY_PATH = libraryPaths;
+
+        shellHook = '''';
+      };
     };
 
-    packages.${system}.default = pkgs.writeShellApplication {
-      name = "mokuro";
-      runtimeInputs = dependencies;
+    packages.${system} = {
+      default = pkgs.writeShellApplication {
+        name = "mokuro";
+        runtimeInputs = dependencies ++ [pythonWithoutPackages];
 
-      text = ''
-        export LD_LIBRARY_PATH=${libraryPaths}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+        text = ''
+          export LD_LIBRARY_PATH=${libraryPaths}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
 
-        # if [ ! -d ".venv" ]; then
-        #   uv venv
-        #   uv sync
-        # fi
+          uv run python -m mokuro "$@"
+        '';
+      };
 
-        uv run python -m mokuro "$@"
-      '';
+      mokuro-nix = pkgs.writeShellApplication {
+        name = "mokuro";
+        runtimeInputs =
+          [pythonWithPackages] ++ dependencies;
+
+        text = ''
+          export LD_LIBRARY_PATH=${libraryPaths}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+
+          python -m mokuro "$@"
+        '';
+      };
     };
 
     apps.${system}.default = {
